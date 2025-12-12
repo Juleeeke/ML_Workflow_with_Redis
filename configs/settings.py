494287fa150f41c85b155cb85248ff0d65cfdbc3
Juleeeke <juleyau@gmail.com>
2025-12-13 02:04:49 +0800

    very first build

---
+++ configs/settings.py
@@ -0,0 +1,17 @@
+# configs/settings.py
+import os
+
+#主机的IP：
+#单设备模拟，以'localhost'
+#多设备，指向主机的IP
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
+REDIS_DB = 0
+
+#Queue keys
+TASK_QUEUE_KEY = 'boston:tasks'
+RESULT_QUEUE_KEY = 'boston:results'
+
+#工作设置
+WORKER_TIMEOUT = 5 
